414f8c00cf1d0162b05039279a890ecd3a0c8911
03shapovalov <161766764+03shapovalov@users.noreply.github.com>
2024-04-05 22:15:51 +0300

    doopracuwaw 2

--- dom2mod3.py
+++ dom2mod3.py
@@ -2,7 +2,7 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     
-    if min < 1 or max > 49 or quantity != 6:
+    if min < 1 or max >= 1001 or quantity < min or quantity > max:
         print("Параметри не коректні!")
         return None
     
@@ -11,5 +11,5 @@ def get_numbers_ticket(min, max, quantity):
         numbers.add(random.randint(min, max))
     return list(numbers)
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(1, 777, 10)
 print("Ваші лотерейні числа:", lottery_numbers)
